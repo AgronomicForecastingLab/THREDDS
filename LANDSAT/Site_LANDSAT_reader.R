62e89c3e57367b1139a07a73f70b6860d7cad9ad
Hamze Dokoohaki <para2x@gmail.com>
2020-11-13 14:48:24 +0000

    Adding metdata and fixing lat/lon

--- LANDSAT/Site_LANDSAT_reader.R
+++ LANDSAT/Site_LANDSAT_reader.R
@@ -2,7 +2,9 @@ library(ncdf4)
 library(tidyverse)
 library(tidync)
 library(furrr)
-plan(multiprocess)
+library(callr)
+plan(multisession, workers = 8)
+setwd("C:/Users/hamzed/GoogleDrive")
 url <- "http://localhost/thredds/dodsC/testAll/NC/"
 #https://opendap.github.io/documentation/QuickStart.html  
 tile.info <- read.csv("https://raw.githubusercontent.com/AgronomicForecastingLab/THREDDS/main/LANDSAT/Tile_extents.csv",
@@ -51,6 +53,7 @@ Becks_sites <- read.csv("C:/Users/hamzed/OneDrive - University of Illinois - Urb
 
 run.threds <- function() {
   tryCatch({
+    Sys.sleep(1)
     RCurl::getURL('localhost:80/thredds/catalog.html')
   }, error = function(cond) {
     print("THREDDS is down !")
@@ -68,39 +71,85 @@ run.threds <- function() {
 Becks_LANDSAT <- Becks_sites %>%
   #head(1) %>%
   dplyr::select(Lat = Latitude, Lon = Longitude, TestPlotId, YearCode) %>%
-  pmap(possibly(function(Lat, Lon, TestPlotId, YearCode) {
+  pwalk(possibly(function(Lat, Lon, TestPlotId, YearCode) {
     print(TestPlotId)
     #Find the tiles that this site will fit in
     avail.img <- tile.info %>%
       filter(Lat > ymin, Lat < ymax, Lon > xmin, Lon < xmax)# %>%
-     # filter(lubridate::year(Date)==YearCode)
+    # filter(lubridate::year(Date)==YearCode)
     
     #if files exists go to the next
-      if(file.exists(paste0("RDS/",TestPlotId, ".RDS"))) return(NULL)
+    if(file.exists(paste0("RDS/",TestPlotId, ".RDS"))) return(NULL)
     
     #check if the thredds is up. Otherwise bring it up
     run.threds()
     
     # Get those tiles from the THREDDS
     data.s <- avail.img$File %>%
-      future_map(possibly(function(.x) {
-        Sys.sleep(runif(1,0,2))
-        OpenDap_LANDSAT(paste0(url,
-                               tools::file_path_sans_ext(.x),
-                               ".nc"),
-                        Lat + 0.001,
-                        Lat,
-                        Lon,
-                        Lon + 0.001) %>% mutate(File = .x)
-      },
-      otherwise = NULL, quiet = FALSE),.progress = TRUE) %>%
-      setNames(avail.img$Date) %>%
-      discard(~ nrow(.x) == 0)
+      future_map2(avail.img$Date, possibly(function(.x, datef) {
+        ncfilen <- paste0(url,
+                          tools::file_path_sans_ext(.x),
+                          ".nc")
+        # send the operation to an external R session to avoid memory leakage
+        callr::r(function(url, Lat, Lon, ncfilen, datef){
+          library(dplyr)
+          OpenDap_LANDSAT <- function(url, ymax, ymin, xmin, xmax, datef=NULL) {
+            
+            # Finding all the lat/long before subsetting
+            lon <- ncdf4::ncvar_get(ncdf4::nc_open(url),'x')
+            lat <- ncdf4::ncvar_get(ncdf4::nc_open(url),'y')
+            
+            
+            #find the index of the bounds 
+            ind.y.min <- which.min(abs(ymax - lat))
+            ind.y.max <- which.min(abs(ymin - lat))
+            ind.x.min <- which.min(abs(xmin - lon))
+            ind.x.max <- which.min(abs(xmax - lon))
+            #make the range of indices
+            ydx <-  ind.y.min:ind.y.max
+            xdx <- ind.x.min:ind.x.max
+            
+            #create the subset url
+            subset.url <-
+              paste0(
+                url,
+                '?elevation[0:1:11][',ind.y.min,':1:',ind.y.max,']',
+                '[',ind.x.min,':1:', ind.x.max ,']'
+              )
+            
+            #read the subset url and replace lat/lon indices to real lat long
+            p <- tidync::tidync(subset.url) %>%
+              tidync::hyper_tibble(na.rm = FALSE) %>%
+              dplyr::mutate(x=x+ind.x.min-1,
+                            y=y+ind.y.min-1)%>%
+              dplyr::select(value=elevation, lon=x, lat=y, band) %>%
+              dplyr::filter(is.finite(value))
+            
+            p$lon<- lon[p$lon]
+            p$lat<- lat[p$lat]
+            
+            return(list(p, MetaData=list(ymax=ymax,
+                                         ymin=ymin,
+                                         xmin=xmin,
+                                         xmax=xmax,
+                                         File=url,
+                                         Date=datef) ))
+          }
+          
+          OpenDap_LANDSAT(ncfilen,
+                          Lat + 0.001,
+                          Lat - 0.001,
+                          Lon - 0.001,
+                          Lon + 0.001, 
+                          datef) 
+          
+        },args=list(url, Lat, Lon, ncfilen, datef), show=FALSE)
+      },otherwise = NULL, quiet = FALSE),.progress = TRUE) 
     
-
+    
     saveRDS(data.s, paste0("RDS/",TestPlotId, ".RDS"))
     #return(data.s)
-    Sys.sleep(2)
+    gc()
   }, otherwise = NULL,
   quiet = FALSE)
   )
